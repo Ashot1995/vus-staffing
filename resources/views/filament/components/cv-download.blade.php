7777619ca3ee3ef455eb5a271610f2168123b7d5
Ashot Gharakeshishyan <n.melkonyan@aico.swiss>
2025-12-21 21:09:02 +0400

    add translate

--- resources/views/filament/components/cv-download.blade.php
+++ resources/views/filament/components/cv-download.blade.php
@@ -32,17 +32,17 @@
             <svg class="w-4 h-4 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 10v6m0 0l-3-3m3 3l3-3m2 8H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"></path>
             </svg>
-            Download CV: {{ $fileName }}
+            {{ __('messages.admin.application.download_cv') }}: {{ $fileName }}
         </a>
         <a href="{{ $url }}" target="_blank" class="inline-flex items-center px-3 py-2 text-sm font-medium text-gray-700 bg-gray-100 rounded-lg hover:bg-gray-200">
             <svg class="w-4 h-4 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path>
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"></path>
             </svg>
-            View CV
+            {{ __('messages.admin.application.view_cv') }}
         </a>
     </div>
 @else
-    <p class="text-sm text-gray-500">No CV uploaded</p>
+    <p class="text-sm text-gray-500">{{ __('messages.admin.application.no_cv') }}</p>
 @endif
 
